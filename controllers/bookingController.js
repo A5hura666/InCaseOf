fd3d0fd94e5bafe04a00a452e385fb858ed77445
Antoine B <aberthillot@myges.fr>
2025-06-16 09:12:49 +0200

    Moved bookings methods to bookingController

---
+++ controllers/bookingController.js
@@ -0,0 +1,60 @@
+const Bookings = require("../models/Booking");
+const {LockerManager} = require("../manager/LockerManager");
+
+
+exports.getBookings = async (req, res) => {
+    try {
+        const fleurs = await Bookings.find();
+        res.status(200).json(fleurs);
+    } catch (err) {
+        res.status(400).json({ error: err });
+    }
+}
+
+exports.postBooking = async (req, res) => {
+    try {
+        const lockerBooked = LockerManager.findLockerById(req.body.lockerId);
+        if (lockerBooked && lockerBooked.lockerStatus === 1) {
+            const newBookings = new Bookings(req.body);
+            const registeredBookings = await newBookings.save();
+            lockerBooked.lockerStatus = 0;
+            lockerBooked.save();
+            res.status(201).json(registeredBookings);
+        } else {
+            res.status(400).json({ error: "Locker not available" });
+        }
+    } catch (err) {
+        res.status(400).json({ error: err });
+    }
+}
+
+exports.getBookingById = async (req, res) => {
+    try {
+        const fleur = await Bookings.findById(req.params.id);
+        res.status(200).json(fleur);
+    } catch (err) {
+        res.status(400).json({ error: err });
+    }
+}
+
+exports.updateBooking = async (req, res) => {
+    try {
+        const updatedBookings = await Bookings.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedBookings);
+    } catch (err) {
+        res.status(400).json({ error: err });
+    }
+}
+
+exports.deleteBooking = async (req, res) => {
+    try {
+        const deletedBookings = await Bookings.findByIdAndDelete(req.params.id);
+        res.status(200).json(deletedBookings);
+    } catch (err) {
+        res.status(400).json({ error: err });
+    }
+}
--- routes/bookings.js
+++ routes/bookings.js
@@ -1,62 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const Bookings = require("../models/Booking");
-const {LockerManager} = require("../manager/LockerManager");
+const bookingController = require("../controllers/bookingController");
 
-router.post("/", async (req, res) => {
-  try {
-    const lockerBooked = LockerManager.findLockerById(req.body.lockerId);
-    if (lockerBooked && lockerBooked.lockerStatus === 1) {
-      const newBookings = new Bookings(req.body);
-      const registeredBookings = await newBookings.save();
-      lockerBooked.lockerStatus = 0;
-      lockerBooked.save();
-      res.status(201).json(registeredBookings);
-    } else {
-      res.status(400).json({ error: "Locker not available" });
-    }
-  } catch (err) {
-    res.status(400).json({ error: err });
-  }
-});
+router.post("/", bookingController.postBooking);
 
-router.get("/", async (req, res) => {
-  try {
-    const fleurs = await Bookings.find();
-    res.status(200).json(fleurs);
-  } catch (err) {
-    res.status(400).json({ error: err });
-  }
-});
+router.get("/", bookingController.getBookings);
 
-router.get("/:id", async (req, res) => {
-  try {
-    const fleur = await Bookings.findById(req.params.id);
-    res.status(200).json(fleur);
-  } catch (err) {
-    res.status(400).json({ error: err });
-  }
-});
+router.get("/:id", bookingController.getBookingById);
 
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedBookings = await Bookings.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-    res.status(200).json(updatedBookings);
-  } catch (err) {
-    res.status(400).json({ error: err });
-  }
-});
+router.put("/:id", bookingController.updateBooking);
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const deletedBookings = await Bookings.findByIdAndDelete(req.params.id);
-    res.status(200).json(deletedBookings);
-  } catch (err) {
-    res.status(400).json({ error: err });
-  }
-});
+router.delete("/:id", bookingController.deleteBooking);
+
 module.exports = router;

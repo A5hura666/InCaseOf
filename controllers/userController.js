3396d9041b230301297a4a774a4f8d1901a66f81
Antoine B <aberthillot@myges.fr>
2025-06-16 09:47:24 +0200

    Moved users methods to userController

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+const User = require('../models/User');
+
+exports.postUser = async (req, res) => {
+    try{
+        const newUser = new User(req.body);
+        const registeredUser = await newUser.save();
+        res.status(200).json(registeredUser);
+    }catch (err) {
+        res.status(400).json({error: err});
+    }
+}
+
+exports.getUsers = async (req, res) => {
+    try{
+        const users = await User.find();
+        res.status(200).json(users);
+    }catch (err) {
+        res.status(400).json({error: err});
+    }
+}
+
+exports.getUserById = async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    }catch (err) {
+        res.status(400).json({error: err});
+    }
+}
+
+exports.updateUser = async (req, res) => {
+    try{
+        const updatedUser = await User.findByIdAndUpdate
+        (req.params.id, req.body, {new: true});
+        res.status(200).json(updatedUser);
+    }catch (err) {
+        res.status(400).json({error: err});
+    }
+}
+
+exports.deleteUser = async (req, res) => {
+    try{
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        res.status(200).json(deletedUser);
+    }catch (err) {
+        res.status(400).json({error: err});
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,15 @@
-var express = require('express');
-var router = express.Router();
-const User = require('../models/User');
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
 
-router.post('/', async (req, res) => {
-  try{
-    const newUser = new User(req.body);
-    const registeredUser = await newUser.save();
-    res.status(200).json(registeredUser);
-  }catch (err) {
-    res.status(400).json({error: err});
-  }
-});
+router.post('/', userController.postUser);
 
-router.get('/', async (req, res) => {
-  try{
-    const users = await User.find();
-    res.status(200).json(users);
-  }catch (err) {
-    res.status(400).json({error: err});
-  }
-});
+router.get('/', userController.getUsers);
 
-router.get('/:id', async (req, res) => {
-  try{
-    const user = await User.findById(req.params.id);
-    res.status(200).json(user);
-  }catch (err) {
-    res.status(400).json({error: err});
-  }
-});
+router.get('/:id', userController.getUserById);
 
-router.put('/:id', async (req, res) => {
-  try{
-    const updatedUser = await User.findByIdAndUpdate
-    (req.params.id, req.body, {new: true});
-    res.status(200).json(updatedUser);
-  }catch (err) {
-    res.status(400).json({error: err});
-  }
-});
+router.put('/:id', userController.updateUser);
 
-router.delete('/:id', async (req, res) => {
-      try{
-        const deletedUser = await User.findByIdAndDelete(req.params.id);
-        res.status(200).json(deletedUser);
-      }catch (err) {
-        res.status(400).json({error: err});
-      }
-    }
-);
+router.delete('/:id', userController.deleteUser);
+
 module.exports = router;

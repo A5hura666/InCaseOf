d07357b66f3800109452c9d3422459009a59b2d3
Antoine B <aberthillot@myges.fr>
2025-06-16 09:32:28 +0200

    Changed connection string

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/api_in_case_of');
+        await mongoose.connect('mongodb://root:example@127.0.0.1:27017/api_in_case_of?authSource=admin');
         console.log('Connexion à MongoDB réussie');
     } catch (error) {
         console.error('Erreur de connexion ', error);

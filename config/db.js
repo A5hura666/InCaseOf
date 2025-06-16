065b6c1623e71ce86a1c5349ab6570cff58b4594
Ethan Bourguigneau <ethan.devfs@pm.me>
2025-06-16 10:36:54 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = async () => {
+    console.log(process.env.MONGO_AUTH_CONNECT_URI)
     try {
         await mongoose.connect(`${process.env.MONGO_AUTH_CONNECT_URI}`);
         console.log('Connexion à MongoDB réussie');

816e8faf858f04ee2aac18bb736b17a16e333ade
Antoine B <aberthillot@myges.fr>
2025-06-16 09:42:44 +0200

    Fixed init script

--- config/init-mongo/init.js
+++ config/init-mongo/init.js
@@ -1,4 +1,4 @@
 db = db.getSiblingDB('api_in_case_of');
-db.createCollection('Users');
-db.createCollection('Lockers');
-db.createCollection('Bookings');
+db.createCollection('users');
+db.createCollection('lockers');
+db.createCollection('bookings');

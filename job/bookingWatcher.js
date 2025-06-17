84e4f729d33deff054a7e9f5b6061f8f010166a9
Antoine B <aberthillot@myges.fr>
2025-06-17 13:59:32 +0200

    booking reminder wip

--- job/bookingWatcher.js
+++ job/bookingWatcher.js
@@ -1,7 +1,57 @@
 const Booking = require('../models/Booking');
+const User = require('../models/User');
 const Locker = require('../models/Locker');
+const {readFileSync} = require("node:fs");
+const {join} = require("node:path");
+const sendEmail = require("../utils/mailer");
+
+async function checkBookingToRemindUser() {
+    console.log("Checking Booking : reminder");
+    const now = new Date();
+
+    try {
+        let options = {
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+        };
+        const lowerBound = new Date(new Date().setMinutes(now.getMinutes() + 31));
+        const upperBound = new Date(new Date().setMinutes(now.getMinutes() + 30));
+
+        console.log("Checking Booking between " + lowerBound.toLocaleDateString("fr-FR", options) + " and " + upperBound.toLocaleDateString("fr-FR", options));
+
+        const bookingsToRemind = await Booking.find({
+            endDate: {$gt: lowerBound, $lte: upperBound}
+        });
+
+        console.log("bookingsToRemind: ", bookingsToRemind);
+
+        for (const booking of bookingsToRemind) {
+            const user = await User.findById(booking.user);
+            const locker = await Locker.findById(booking.locker)
+
+            const emailTemplatePath = join(__dirname, '..', 'views', 'emails', 'reminder-expiration');
+            let emailHTML = readFileSync(emailTemplatePath, 'utf8');
+            emailHTML = emailHTML.replace('{{userName}}', user.firstName).replace('{{lockerNumber}}', locker.lockerNumber);
+
+            await sendEmail({
+                to: user.email,
+                subject: 'Rappel : votre réservation arrive à expiration',
+                html: emailHTML
+            });
+
+            console.log('Sent reminder email to ', user.firstName);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 async function checkExpiredBookings() {
+    console.log("Checking Booking : expiration");
     const now = new Date();
 
     try {
@@ -22,5 +72,8 @@ async function checkExpiredBookings() {
 }
 
 module.exports = function startBookingWatcher(intervalMs = 60 * 1000) {
+    checkExpiredBookings();
+    checkBookingToRemindUser();
     setInterval(checkExpiredBookings, intervalMs);
+    setInterval(checkBookingToRemindUser, intervalMs);
 };
